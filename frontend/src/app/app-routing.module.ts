6fbd60e765c620a5a93433b21f05ef234667e693
NoemiMatiasReynoso <noemimatias384@gmail.com>
2023-06-12 14:16:45 +0200

    se hizo ruta de weather

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WeatherModule } from './weather/weather.module';
 
 const routes: Routes = [
   {
@@ -9,6 +10,10 @@ const routes: Routes = [
   {
     path: 'users',
     loadChildren: () => import('./users/users.module').then(m => m.UsersModule)
+  },
+  {
+    path: 'weather',
+    loadChildren: () => import('./users/users.module').then(m =>WeatherModule)
   }
 ];
 
