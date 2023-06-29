b6e024345541e83aad7c8e303b33ff7927076a32
caroldmg <carolinadmg98@gmail.com>
2023-06-29 13:01:45 +0200

    backend - crear módulo towns

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { ProvincesModule } from './provinces/provinces.module';
+import { TownsModule } from './towns/towns.module';
 
 @Module({
-  imports: [DatabaseModule, ProvincesModule],
+  imports: [DatabaseModule, ProvincesModule, TownsModule],
   controllers: [AppController],
   providers: [AppService],
 })
