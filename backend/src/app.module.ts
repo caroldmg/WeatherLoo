87f5b4c124bf789cef01b3e2569595807a66cc13
caroldmg <carolinadmg98@gmail.com>
2023-06-29 12:59:42 +0200

    backend- crear módulo provinces

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { ProvincesModule } from './provinces/provinces.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, ProvincesModule],
   controllers: [AppController],
   providers: [AppService],
 })
