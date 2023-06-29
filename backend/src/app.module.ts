7793e9204b972e75b180b6ed10d09a124f7c99c9
caroldmg <carolinadmg98@gmail.com>
2023-06-29 13:04:16 +0200

    backend - crear módulo users

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { ProvincesModule } from './provinces/provinces.module';
 import { TownsModule } from './towns/towns.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [DatabaseModule, ProvincesModule, TownsModule],
+  imports: [DatabaseModule, ProvincesModule, TownsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
