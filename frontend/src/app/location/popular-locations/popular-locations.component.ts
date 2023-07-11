6c31f0412163c0cdea2b9b0c6920d796c985cf41
Maga <magalisantiago@hotmail.com>
2023-07-11 22:49:35 +0200

    Agregado de img nube, y css para ejemplos de componente detalleDelDia

--- frontend/src/app/location/popular-locations/popular-locations.component.ts
+++ frontend/src/app/location/popular-locations/popular-locations.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ITown } from '../models/town.model';
+import { ActivatedRoute } from '@angular/router';
+import { LocationService } from '../services/location.service';
 
 @Component({
   selector: 'app-popular-locations',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./popular-locations.component.css']
 })
 export class PopularLocationsComponent {
+
+  municipio: ITown | undefined
+  
+  constructor(private activatedRoute: ActivatedRoute,
+              private locationService: LocationService){}
+
+ // ngOnInit(): void {
+     //this.loadLocations();
+   
+    // }
+    
+  
+  //   loadLocations(){    
+  //   this.activatedRoute.params.subscribe((params) => {     
+     // const townCode = params['townCode']
+//this.locationService.getWeatherRealTime(townCode).subscribe(data => this.weather = data)
+   //})
+
+ //}
 
 }
