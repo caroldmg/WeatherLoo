98170d6b11bc684a68a03f19f12ab171867fa86f
caroldmg <carolinadmg98@gmail.com>
2023-07-13 16:45:02 +0200

    backend: servicios y controllers de town y provinces

--- backend/src/towns/towns.controller.ts
+++ backend/src/towns/towns.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { TownsService } from './towns.service';
+import { Town } from './towns.entity';
 
 @Controller('towns')
-export class TownsController {}
+export class TownsController {
+
+    constructor(private townService: TownsService){}
+
+    @Get()
+    findAll(): Promise <Town[]>{
+        return this.townService.findAll()
+    }
+
+    @Get('province/:provinceId')
+    findAllByAutonomyId(
+        @Param("provinceId") provinceId: string): Promise <Town[]>{
+            return this.townService.findAllByProvinceId(provinceId)
+    }
+
+    @Get('townCode/:townCode')
+    findById(@Param('townCode') townCode: string): Promise <Town>{
+        return this.townService.findById(townCode)
+    }
+
+}
--- backend/src/towns/towns.service.ts
+++ backend/src/towns/towns.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Town } from './towns.entity';
 
 @Injectable()
-export class TownsService {}
+export class TownsService {
+    constructor(
+        @InjectRepository(Town) private TownRepo: Repository<Town>
+    ){}
+
+    findAll(): Promise<Town[]>{ //con relaciones
+        return this.TownRepo.find({
+            relations: {
+                province: true
+            }
+        })
+    }
+
+
+
+    findById(townCode: string): Promise <Town | null>{
+        return this.TownRepo.findOne({
+            where: {
+                townCode: townCode
+            }
+        })
+    }
+
+    findAllByProvinceId(provinceId: string): Promise<Town[]>{
+        return this.TownRepo.find({
+            relations: {
+                province: true
+            },
+            where: {
+                province: {
+                    id: provinceId
+                }
+            }
+        })
+    }
+}
