161ccb970e0c77b76d8dd376266962c81daec40c
caroldmg <carolinadmg98@gmail.com>
2023-07-13 17:07:35 +0200

    backend: autonomyService y controller

--- backend/src/autonomy/autonomy.service.ts
+++ backend/src/autonomy/autonomy.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Autonomy } from './autonomy.entity';
 
 @Injectable()
-export class AutonomyService {}
+export class AutonomyService {
+
+    constructor(
+        @InjectRepository(Autonomy) private autonomyRepo: Repository<Autonomy>
+    ){}
+
+    findAll(): Promise<Autonomy[]>{ //con relaciones
+        return this.autonomyRepo.find()
+    }
+
+    findById(autonomyId: string): Promise <Autonomy | null>{
+        return this.autonomyRepo.findOne({
+            where: {
+                autonomyId: autonomyId
+            }
+        })
+    }
+
+}
+
--- backend/src/towns/towns.service.ts
+++ backend/src/towns/towns.service.ts
@@ -6,11 +6,11 @@ import { Town } from './towns.entity';
 @Injectable()
 export class TownsService {
     constructor(
-        @InjectRepository(Town) private TownRepo: Repository<Town>
+        @InjectRepository(Town) private townRepo: Repository<Town>
     ){}
 
     findAll(): Promise<Town[]>{ //con relaciones
-        return this.TownRepo.find({
+        return this.townRepo.find({
             relations: {
                 province: true
             }
@@ -20,7 +20,7 @@ export class TownsService {
 
 
     findById(townCode: string): Promise <Town | null>{
-        return this.TownRepo.findOne({
+        return this.townRepo.findOne({
             where: {
                 townCode: townCode
             }
@@ -28,7 +28,7 @@ export class TownsService {
     }
 
     findAllByProvinceId(provinceId: string): Promise<Town[]>{
-        return this.TownRepo.find({
+        return this.townRepo.find({
             relations: {
                 province: true
             },
