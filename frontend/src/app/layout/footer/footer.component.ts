032d22296c0d85bab8b26841cece884eb791a1dc
Maga <magalisantiago@hotmail.com>
2023-07-30 02:54:14 +0200

    Frontend footer css avances

--- frontend/src/app/layout/footer/footer.component.ts
+++ frontend/src/app/layout/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  
+
+  showMenu: boolean = false
+  isLoggedIn = false;
+
+  constructor(public authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.isLoggedIn.subscribe(loggedIn => this.isLoggedIn = loggedIn);
+}
 
 }
