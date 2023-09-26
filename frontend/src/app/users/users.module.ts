8fde40a7e8cf14796dad9377038b5c4f8e3b66f3
caroldmg <caroldmg98@gmail.com>
2023-09-26 17:34:00 +0200

    cambio estilo en user-form

--- frontend/src/app/users/users.module.ts
+++ frontend/src/app/users/users.module.ts
@@ -14,13 +14,13 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatStepperModule} from '@angular/material/stepper';
 import {MatButtonModule} from '@angular/material/button';
-import {MatRadioModule} from '@angular/material/radio';
+import {MAT_RADIO_DEFAULT_OPTIONS, MatRadioModule} from '@angular/material/radio';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
 import { HttpClientModule } from '@angular/common/http';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatListModule } from '@angular/material/list';
-import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MAT_CHECKBOX_DEFAULT_OPTIONS, MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import {MatDividerModule} from '@angular/material/divider';
 
@@ -54,7 +54,9 @@ import { LocationModule } from '../location/location.module';
     LocationModule
   ],
   providers: [
-    { provide:  MAT_DATE_LOCALE, useValue : 'es-ES'}
+    { provide:  MAT_DATE_LOCALE, useValue : 'es-ES' },
+    {provide: MAT_RADIO_DEFAULT_OPTIONS, useValue: {color: 'primary'}},
+    {provide: MAT_CHECKBOX_DEFAULT_OPTIONS, useValue: {color: 'primary'} }
   ]
 })
 export class UsersModule { 
